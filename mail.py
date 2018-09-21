80431f49a5cf40bca35a65a18e045f18c7e9503f
Prashant Kumar <kr.prashant94@gmail.com>
2018-09-21 15:17:02 +0530

    New editing class added

--- mail.py
+++ mail.py
@@ -19,16 +19,62 @@ from PIL import ImageDraw
 import pandas as pd
 import datetime
 
+class Editor:
+    """docstring for Editor"""
+    def __init__(self, tamplate_file):
+        """
+        Template location
+        """
+        self.tamplate_file = tamplate_file
+        self.img = Image.open(tamplate_file)
+        self.width, self.height = self.img.size
+        self.draw = ImageDraw.Draw(self.img)
+        self.font = ImageFont.truetype('a.ttf', 150)
+        self.location = (0,0)
+    def setFont(self, font_name, size):
+        """
+        Set font style and font size
+        """
+        self.font = ImageFont.truetype(font_name, size)
+    def setLocation(self, x, y):
+        """
+        Set cursor location where to write. Right alignment
+        """
+        self.location = (x, y)
+    def writeName(self, name, color=(0,0,0)):
+        """
+        Write the name of certificate
+        """
+        self.draw.text(self.location, name, color,font=self.font)
+    def save(self, file_name):
+        """
+        Save the edited file
+        """
+        self.img.save('Generated/'+file_name+'.png')
+
 
 # set up the SMTP server
-
-class gmailMailler():
-    me = ""
-    password = ""
-
-    server = ""
+class Mailler():
+    """
+    Mailler class
+    """
 
     def __init__(self, mail, password):
+        self.html = """\
+        <html>
+          <head></head>
+          <body>
+            <p>Hi!<br> 
+               How are you?<br> 
+               Here is the <a href="https://www.python.org">link</a> you wanted.
+            </p>
+          </body>
+        </html>"""
+        self.sub = "Test : Certificate of Participation"
+        
+        self.me = ""
+        self.password = ""
+        self.server = ""
         self.me = mail
         self.password = password
 
@@ -55,38 +101,31 @@ class gmailMailler():
         self.server.sendmail(self.me, to, msg.as_string())
 
 
-
-html = '\
-<html>\
-  <head></head>\
-  <body>\
-    <p>Hi!<br> \
-       How are you?<br> \
-       Here is the <a href="https://www.python.org">link</a> you wanted.\
-    </p>\
-  </body>\
-</html>'
-sub = "Test : Certificate of Participation of PINK (Rotaract club of RCCIIT)"
+if not os.path.exists('Generated'):
+    os.makedirs('Generated')
 
 initTime = datetime.datetime.now()
 no = 0
-names = pd.read_csv("pink.csv")
-#print(len(names))
+names = pd.read_csv("mailling_list.csv")
+# print(names)
 
-for i in range(0 , len(names)):
-    img = Image.open("certificate.png")
-    width, height = img.size
-    draw = ImageDraw.Draw(img)
-    # font = ImageFont.truetype(<font-file>, <font-size>)
-    font = ImageFont.truetype("a.ttf", 150)
-    # draw.text((x, y),"Sample Text",(r,g,b))
-    location = width/2 - 45*len(names.loc[i]['name'])
-    draw.text((location, 1300),names.loc[i]['name'].upper(),(0,0,0),font=font)
-    img.save('Generated/'+names.loc[i]['mail'].lower()+'.png')
 
-    mailler = gmailMailler("<mail-id>", "<password>")
+for index, row in names.iterrows():
+    certi = Editor("certificate.png")
+    certi.setFont("a.ttf", 150)
+    certi.setLocation(certi.width/2 - 45*len(row['name']), 1300)
+    certi.writeName(row['name'].upper())
+    certi.save(row['mail'])
+    
+    f = open('account.config', 'r')
+    info = f.read()
+    f.close()
+    info = info.split()
+    print(info)
+    continue
+    mailler = gmailMailler(info[0], info[1])
     mailler.open()
-    TODO: Change it to names.loc[i]['mail'].lower()
+    # TODO: Change it to names.loc[i]['mail'].lower()
     mailler.send("<mail-id>", sub, html, "Generated/"+names.loc[i]['mail'].lower()+".png")
     mailler.close()
     no = i
